26a322cc179f68524c0735721bad63b06729c880
Daniel Chabowski <you@example.com>
2023-03-31 20:51:05 +0200

    Task 2 completed

---
+++ Bmi2/app/src/main/java/com/project/bmi2/Activity1.java
@@ -0,0 +1,99 @@
+package com.project.bmi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Activity1 extends AppCompatActivity {
+
+    private double weight = 0.0;
+    private double height = 0.15;
+    //TextViews definitions
+    private TextView heightTextView;
+    private TextView weightTextView;
+    private TextView totalTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+        weightTextView = (TextView) findViewById(R.id.weightTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        totalTextView.setText(String.format(0 + ""));
+
+        EditText weightEditText =
+                (EditText) findViewById(R.id.weightEditText);
+        weightEditText.addTextChangedListener(weightEditTextWatcher);
+        EditText heightEditText =
+                (EditText) findViewById(R.id.heightEditText);
+        heightEditText.addTextChangedListener(heightEditTextWatcher);
+
+
+    }
+
+    private void calculate() {
+
+
+        double total = weight / (height * height);
+
+
+        totalTextView.setText(String.format(total + ""));
+    }
+
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                weight = Double.parseDouble(s.toString()) / 100.0;
+                weightTextView.setText(String.format(weight + ""));
+            } catch (NumberFormatException e) {
+                weightTextView.setText("");
+                weight = 0.0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                height = Double.parseDouble(s.toString()) / 100.0;
+                heightTextView.setText(String.format(height + ""));
+            } catch (NumberFormatException e) {
+                heightTextView.setText("");
+                height = 0.0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+}
